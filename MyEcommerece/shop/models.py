1f5c83cd44f203115ff835e546baeee8d9780e44
Nilay Kanire <nilaykanire@gmail.com>
2024-11-25 01:22:14 +0530

    Models in Django (Django Tut #24) :-
    1. Created 'models.py' file in shop app. Then created a class names as "Product" and applied inheritance in that class.
    
    2. learned about migrations
    
    3. Here in django 'sqlite3' database is used.
    
    4. In settings.py at Installed apps, if we make changes then instead of migrating directly to the database, I first store the migration elsewhere so that the changes doesn't get written in database (for which i used terminal command 'python manage.py makemigrations'. Afterwards when i felt like the made changes are fine, i made the migration and wrote it in database using syntax 'python manage.py migrate'.

--- MyEcommerece/shop/models.py
+++ MyEcommerece/shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):        #Created new class and applied inheritance here
+    product_id = models.AutoField  # AutoField - An IntegerField that automatically increments according to available IDs.
+    product_name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=300)
+    pub_date = models.DateField()
