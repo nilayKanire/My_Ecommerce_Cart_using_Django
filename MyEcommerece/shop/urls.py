dc34df0d1388fbf22feb4946dee43f52f07e796c
Nilay Kanire <nilaykanire@gmail.com>
2024-11-25 00:33:39 +0530

    Project Layout (Django Tut #23) :-
    1. Here I made changes in urls.py and views.py inside the shop app.
    
    2. urls.py -> provided path to different endpoints here.
    
    3. views.py -> created functions related to our endpoints which were present in the urls.py

--- MyEcommerece/shop/urls.py
+++ MyEcommerece/shop/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="ShopHome")
+    path("", views.index, name="ShopHome"),
+    path("about/", views.index, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("search/", views.search, name="Search"),
+    path("productview/", views.prodView, name="Produts"),
+    path("checkout/", views.checkout, name="Checkout"),
 ]
--- MyEcommerece/shop/views.py
+++ MyEcommerece/shop/views.py
@@ -9,3 +9,18 @@ from django.http import HttpResponse
 # Create views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def contact(request):
+    return HttpResponse("we are at contact")
+
+def tracker(request):
+    return HttpResponse("we are at tracker")
+
+def search(request):
+    return HttpResponse("we are at search")
+
+def prodView(request):
+    return HttpResponse("we are at product view")
+
+def checkout(request):
+    return HttpResponse("we are at checkout")
