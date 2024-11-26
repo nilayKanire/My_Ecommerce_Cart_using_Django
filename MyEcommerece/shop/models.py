8747ec41c78c7592266bc2b9742909da9eacc8f6
Nilay Kanire <nilaykanire@gmail.com>
2024-11-26 17:19:36 +0530

    Adding Images & Changing Models (Django Tut #26) :-
    1. Done some changes in the shop/models.py file
    
    2. Created a method. No migration needed when we created "Method". it need only when we create a model or update it.
    
    3. Note :- 1] Whatever the changes we make in our created database/model , after that we need to migrate changes using the command 'python manage.py makemigrations'.
          2] If we create any table in models.py then we have to register our model in "admin.py" file.
    
    4. Added few variables in the 'Product model'

--- MyEcommerece/shop/models.py
+++ MyEcommerece/shop/models.py
@@ -5,7 +5,19 @@ from django.db import models
 class Product(models.Model):        #Created new class and applied inheritance here
     product_id = models.AutoField  # AutoField - An IntegerField that automatically increments according to available IDs.
     product_name = models.CharField(max_length=50)
-    desc = models.CharField(max_length=300)
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default="0")
+    desc = models.CharField(max_length=300 )
     pub_date = models.DateField()
+    image = models.ImageField(upload_to="shop/images", default="")
 
-''' Note :- If we create any table in models.py then we have to register our model in "admin.py" file'''
+    # Creating method below. no migration is need when we created "Method". it need only when we create a model or update it.
+    def __str__(self):
+        return self.product_name    # over-writes the product name in admin page.
+
+''' Note :- 1] Whatever the changes we make in our created database/model , after that we need to migrate changes using the command 'python manage.py makemigrations'.
+            2] If we create any table in models.py then we have to register our model in "admin.py" file'''
+
+
+
