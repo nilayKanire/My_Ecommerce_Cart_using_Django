a0047714eb682cc492411d0ffb74dafd5ab73833
Nilay Kanire <nilaykanire@gmail.com>
2024-11-26 13:27:32 +0530

    Django Admin & Adding Data To the Database (Django Tut #25) :-
    1. To create Super User in Django project  ( in windows it is called as 'Admin' and in Linux it is called as 'Root user'.
    	-> python manage.py createsuperuser
    
    2. As I have created a table or database in shop/models.py , I had to register that table to shop/admin.py
    
    3. After that i ran the code and in browser went to /admin endpoint. There i logged in and add products there.

--- MyEcommerece/shop/models.py
+++ MyEcommerece/shop/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# Models me database create kar raha hai product naam ka.
 class Product(models.Model):        #Created new class and applied inheritance here
     product_id = models.AutoField  # AutoField - An IntegerField that automatically increments according to available IDs.
     product_name = models.CharField(max_length=50)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
+
+''' Note :- If we create any table in models.py then we have to register our model in "admin.py" file'''
