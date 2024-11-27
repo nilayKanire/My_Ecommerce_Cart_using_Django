41fa3d9c1e8a2280a89776852c466a8335c38279
Nilay Kanire <nilaykanire@gmail.com>
2024-11-27 16:44:09 +0530

    Template Inheritance(Django Tut #31) :-
    1. Created 3 new files, about.html,basic.html and initial_index.html
    
    2. done some minor changes in shop/urls.py and shop/views.py
    
    3. Used Template inheritance sytanx in index.html and basic.html and inheritted template in about.html

--- MyEcommerece/shop/views.py
+++ MyEcommerece/shop/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-#
+
 # Create views here.
 def index(request):
     return render(request, 'shop/index.html')
