9ec1297cfc5b0c0bb5e0dc2e4db8ef03d2d160b9
Nilay Kanire <nilaykanire@gmail.com>
2024-11-27 16:30:41 +0530

    Template Inheritance(Django Tut #31) :-
    1. Created 3 new files, about.html,basic.html and initial_index.html
    
    2. done some minor changes in shop/urls.py and shop/views.py
    
    3. Used Template inheritance sytanx in index.html and basic.html and inheritted template in about.html

--- MyEcommerece/shop/urls.py
+++ MyEcommerece/shop/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="ShopHome"),
-    path("about/", views.index, name="AboutUs"),
+    path("about/", views.about, name="AboutUs"),
     path("contact/", views.contact, name="ContactUs"),
     path("tracker/", views.tracker, name="TrackingStatus"),
     path("search/", views.search, name="Search"),
--- MyEcommerece/shop/views.py
+++ MyEcommerece/shop/views.py
@@ -10,6 +10,9 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'shop/index.html')
 
+def about(request):
+    return render(request, 'shop/about.html')
+
 def contact(request):
     return HttpResponse("we are at contact")
 
